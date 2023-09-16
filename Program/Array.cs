0b13c4035afd1bc8aaf2a3db7cabaa15039fcd8a
rostikcheremys <rostikcheremys@gmail.com>
2023-09-16 13:34:33 +0300

    My Task

---
+++ Program/Array.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Program
+{
+    class Array
+    {
+        private int[] array;
+        private Random random = new Random();
+
+        public Array(int size)
+        {
+            array = new int[size];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < array.Length)
+                {
+                    return array[index];
+                }
+
+                Console.WriteLine("Iндекс виходить за межi масиву!");
+                
+                return 0;
+            }
+        }
+        
+        public int FindMaxElement()
+        {
+            int max = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+
+        public int FindMinElement()
+        {
+            int min = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+        
+        public void FillRandom()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Елемент {i}: {array[i] = random.Next(1, 100)}");
+            }
+        }
+
+        public void FillFromConsole()
+        {
+            Console.WriteLine("Введiть елементи масиву:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Елемент {i}: ");
+                
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        
+        public void ScreenOutput()
+        {
+            foreach (var number in array)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Оберiть спосiб заповнення масиву:");
+            Console.WriteLine("Введiть 1 для вводу з клавiатури)");
+            Console.WriteLine("Введiть 2 для вводу рандомно)");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введiть розмiр масиву: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            Array array = new Array(size);
+                
+            switch (choice)
+            {
+                case 1:
+                    array.FillFromConsole();
+                    break;
+                
+                case 2 :
+                    array.FillRandom();
+                    break;
+                
+                default:
+                    Console.WriteLine("Неправильний вибiр!");
+                    break;
+            }
+                
+            Console.WriteLine("Масив:");
+            array.ScreenOutput();
+
+            Console.WriteLine($"Максимальний елемент: {array.FindMaxElement()}");
+            Console.WriteLine($"Мiнiмальний елемент: {array.FindMinElement()}");
+
+            Console.Write("Введiть iндекс для отримання значення: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine($"Значення за iндексом {index}: {array[index]}");
+        }
+    }
+}
